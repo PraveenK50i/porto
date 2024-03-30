083335f0e1640c3f78e309ddca82f6e37d63dc9c
Praveenk50i <praveensunki1234@gmail.com>
2024-03-30 06:16:13 +0530

    analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Navbar } from "../navbar/navigation";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} overflow-y-scroll overflow-x-hidden`}>
         <Navbar />
         {children}
+        <Analytics />
         </body>
     </html>
   );
